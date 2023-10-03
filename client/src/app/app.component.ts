1fd4a21e7e9d566315d439162f9426820b36554c
nihalprabhu <nihalprabhugaunkar@gmail.com>
2023-10-03 11:25:20 +0530

    Added Login and Register functionality in client app

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from './services/account.service';
+import { User } from './models/users';
 
 @Component({
   selector: 'app-root',
@@ -9,18 +11,19 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   
   title = 'Dating App';
-  users:any;
 
-  constructor(private http: HttpClient) {}
+  constructor( private accountService: AccountService) {}
   ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/users')
-             .subscribe(
-                {
-                  next: response => this.users=response,
-                  error: error=> console.log(error),
-                  complete: ()=> console.log("Request has completed")
-                }
-             )
+    this.setCurrentUser();
+  }
+
+  
+
+  setCurrentUser(){
+    const userString = localStorage.getItem('user')
+    if(!userString) return
+    const user: User = JSON.parse(userString);
+    this.accountService.setCurrentUser(user);
   }
   
 }
